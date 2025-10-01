372ae09bafc352447b1b9734fc4a7f51ecdff9aa
gulizarcicekilgor <gulizarcicek28@gmail.com>
2025-10-01 14:35:24 +0300

    first commit

---
+++ src/test/java/tests/Test1.java
@@ -0,0 +1,49 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import javax.xml.xpath.XPath;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.setProperty("chromeDriver","/drivers/chromedriver");
+        WebDriver driver = new ChromeDriver(); // driver objesi
+
+
+        driver.get("https://demoqa.com/text-box");
+        driver.manage().window().maximize(); //
+
+        WebElement username = driver.findElement(By.id("userName"));
+        username.click();
+        username.sendKeys("Gülizar");
+
+        WebElement userEmail= driver.findElement(By.cssSelector("input#userEmail"));
+        userEmail.click();
+        userEmail.sendKeys("gulizarciek@gmail.com");
+
+        WebElement currentAddress = driver.findElement(By.cssSelector("textarea#currentAddress"));
+        currentAddress.click();//
+        currentAddress.sendKeys("İzmir Buca");
+
+        WebElement permanentAddress = driver.findElement(By.id("permanentAddress"));
+        permanentAddress.click();
+        permanentAddress.sendKeys("Yozgat Merkez");
+
+        WebElement submitBtn = driver.findElement(By.xpath("//button[@id='submit']"));
+        submitBtn.click();
+
+
+        WebElement getname = driver.findElement(By.xpath("//div/p[@id='name']"));
+        System.out.println(getname.getText());
+
+
+
+
+
+    }
+
+
+}
